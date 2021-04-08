fc25925741148bb191dc6c7e952bc28992cdd96f
Gasheezy <mungai.gachango@gmail.com>
2021-04-08 14:27:26 +0300

    first works

--- helloworld.kt
+++ helloworld.kt
@@ -1,3 +1,7 @@
+// fun main() {
+//     println("Hello World!")
+// }
+
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
---
+++ practice.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val test = 10
+    println(test+10)
+
+    val myName = "Mungai"
+    println("My name is " +myName)
+}
